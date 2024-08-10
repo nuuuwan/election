451d899ce76287e96819f8026b57a318315bd06c
nuuuwan <nuuuwan@gmail.com>
2024-08-10 21:47:09 +0530

    various

--- src/view/molecules/PartyToVotesView/PartyToVotesView.js
+++ src/view/molecules/PartyToVotesView/PartyToVotesView.js
@@ -9,11 +9,6 @@ export default function PartyToVotesView({ partyToVotes }) {
 
   return (
     <Stack width="100%" direction="column" gap={1} sx={{ textAlign: "center" }}>
-      <LabelledStat
-        label={`${partyToVotes.winningPartyID} - 50%`}
-        valueStr={Format.intHumanizeSigned(partyToVotes.votesInMajority)}
-        sx={{ color: Party.fromID(partyToVotes.winningPartyID).color }}
-      />
       <PartyToVotesPieChart partyToVotes={partyToVotes} />
 
       {entries.map(function ([partyID, votes], i) {

d6f94409061be6a93336169df5e1e397cf360a64
nuuuwan <nuuuwan@gmail.com>
2024-08-11 18:12:16 +0530

    various fixes; comments;

--- src/nonview/core/ElectionModel/ElectionModel.js
+++ src/nonview/core/ElectionModel/ElectionModel.js
@@ -1,7 +1,7 @@
-import { MLModel, MathX } from "../base";
-import Election from "./Election/Election";
-import PartyToVotes from "./PartyToVotes";
-import Result from "./Result";
+import { MLModel, MathX } from "../../base";
+import Election from "../Election/Election";
+import PartyToVotes from "../PartyToVotes";
+import Result from "../Result";
 
 export default class ElectionModel {
   static MIN_RESULTS_FOR_PREDICTION = 1;
@@ -16,39 +16,44 @@ export default class ElectionModel {
     this.releasedPDIDList = releasedPDIDList;
     this.nonReleasedPDIDList = notReleasePDIDList;
 
-    this.train();
+    this.trainingOutput = this.train();
   }
   static getPartyIDList(modelElection) {
+    // Returns the list of party IDs, where the party has won at least MIN_P votes in the election.
+    const MIN_P = 0.01;
     const resultsList = modelElection.pdResultsList;
     const aggrResults = Result.fromList("aggr", resultsList);
     const partyToPVotesSorted = aggrResults.partyToVotes.partyToPVotesSorted;
     return Object.entries(partyToPVotesSorted)
       .filter(function ([partyID, pVotes]) {
-        return pVotes > 0.05;
+        return pVotes >= MIN_P;
       })
       .map(function ([partyID, pVotes]) {
         return partyID;
       });
   }
 
-  static getFeatureVector(modelElection, partyID, pdIDList) {
+  static getFeatureVector(election, partyID, pdIDList) {
+    // Returns a vector with the % of votes party(ID) has got for pdIDList, in election election.
     return pdIDList.map(function (pdID) {
-      const pdResult = modelElection.getResults(pdID);
+      const pdResult = election.getResults(pdID);
       if (!pdResult) {
         return 0.0;
       }
-      const partyToPVotesSorted = pdResult.partyToVotes.partyToPVotesSorted;
-      return partyToPVotesSorted[partyID] || 0.0;
+      return pdResult.partyToVotes.partyToPVotesSorted[partyID] || 0.0;
     });
   }
 
   static getFeatureMatrixForElection(modelElection, pdIDLIst) {
-    return ElectionModel.getPartyIDList(modelElection).map(function (partyID) {
+    // Returns a matrix, where each row is getFeatureVector for a party in the election.
+    const partyIDList = ElectionModel.getPartyIDList(modelElection);
+    return partyIDList.map(function (partyID) {
       return ElectionModel.getFeatureVector(modelElection, partyID, pdIDLIst);
     });
   }
 
   static getFeatureMatrix(elections, pdIDList) {
+    // Concatenates all feature matrices for all the training elections.
     return elections.reduce(function (X, election) {
       return X.concat(
         ElectionModel.getFeatureMatrixForElection(election, pdIDList)
@@ -56,26 +61,10 @@ export default class ElectionModel {
     }, []);
   }
 
-  static normalizeSingle(partyToVoteInfo) {
-    const totalPVotes = MathX.sum(
-      Object.values(partyToVoteInfo).map((x) => x.pVotesPredicted)
-    );
-    return Object.fromEntries(
-      Object.entries(partyToVoteInfo).map(function ([partyID, voteInfo]) {
-        voteInfo.pVotesPredicted = voteInfo.pVotesPredicted / totalPVotes;
-        return [partyID, voteInfo];
-      })
-    );
-  }
-
-  static normalize(pdToPartyToVoteInfo) {
-    return Object.fromEntries(
-      Object.entries(pdToPartyToVoteInfo).map(function ([
-        pdID,
-        partyToVoteInfo,
-      ]) {
-        return [pdID, ElectionModel.normalizeSingle(partyToVoteInfo)];
-      })
+  getPreviousElections() {
+    // All elections before currentElection.
+    return this.elections.filter(
+      (election) => election.localeCompare(this.currentElection) < 0
     );
   }
 
@@ -93,12 +82,6 @@ export default class ElectionModel {
     );
   }
 
-  getPreviousElections() {
-    return this.elections.filter(
-      (election) => election.localeCompare(this.currentElection) < 0
-    );
-  }
-
   getXEvaluate() {
     return ElectionModel.getFeatureMatrix(
       [this.currentElection],
@@ -113,6 +96,25 @@ export default class ElectionModel {
     );
   }
 
+  static normalizeSingle(partyToPVotes) {
+    const totalPVotes = MathX.sumValues(partyToPVotes);
+    return Object.fromEntries(
+      Object.entries(partyToPVotes).map(function ([partyID, pVotes]) {
+        return [partyID, pVotes / totalPVotes];
+      })
+    );
+  }
+
+  static normalize(pdToPartyToVoteInfo) {
+    return Object.fromEntries(
+      Object.entries(pdToPartyToVoteInfo).map(function ([
+        pdID,
+        partyToVoteInfo,
+      ]) {
+        return [pdID, ElectionModel.normalizeSingle(partyToVoteInfo)];
+      })
+    );
+  }
   train() {
     const XTrain = this.getXTrain();
     const YTrain = this.getYTrain();
@@ -120,53 +122,37 @@ export default class ElectionModel {
     const model = new MLModel(XTrain, YTrain);
 
     const XEvaluate = this.getXEvaluate();
-    const YActual = this.getYActual();
+
     const YHat = XEvaluate.map((Xi) => model.predict(Xi));
     const partyIDList = ElectionModel.getPartyIDList(
       this.currentElection,
       this.releasedPDIDList
     );
 
-    const pdToPartyToVoteInfo = YHat.reduce(
-      function (pdTopartyToPVotesSorted, Yi, i) {
+    const pdToPartyToPVotes = YHat.reduce(
+      function (pdToPartyToPVotes, Yi, i) {
         const partyID = partyIDList[i];
         return Yi.reduce(
-          function (pdTopartyToPVotesSorted, pVotes, j) {
+          function (pdToPartyToPVotes, pVotes, j) {
             const pdID = this.nonReleasedPDIDList[j];
 
-            if (!pdTopartyToPVotesSorted[pdID]) {
-              pdTopartyToPVotesSorted[pdID] = {};
+            if (!pdToPartyToPVotes[pdID]) {
+              pdToPartyToPVotes[pdID] = {};
             }
-            pdTopartyToPVotesSorted[pdID][partyID] = {
-              pVotesPredicted: pVotes,
-              pVotesActual: YActual[i][j],
-            };
-            return pdTopartyToPVotesSorted;
+            pdToPartyToPVotes[pdID][partyID] = pVotes;
+            return pdToPartyToPVotes;
           }.bind(this),
-          pdTopartyToPVotesSorted
+          pdToPartyToPVotes
         );
       }.bind(this),
       {}
     );
-
-    return ElectionModel.normalize(pdToPartyToVoteInfo);
-  }
-
-  getElectionReleased() {
-    let election = new Election(
-      this.currentElection.electionType,
-      this.currentElection.date
-    );
-    election.resultsList = this.releasedPDIDList
-      .map((pdID) => this.currentElection.getResults(pdID))
-      .filter((result) => result);
-    election.resultsIdx = Election.buildResultsIdx(election.resultsList);
-    election.isLoaded = true;
-    return election;
+    return ElectionModel.normalize(pdToPartyToPVotes);
   }
 
   getElectionNotReleasedPrediction() {
-    const normPDTopartyToPVotesSorted = this.train();
+    console.debug("getElectionNotReleasedPrediction");
+    const normPDToPartyToPVotes = this.trainingOutput;
     const lastElection = this.getPreviousElections().slice(-1)[0];
 
     let election = new Election(
@@ -182,10 +168,10 @@ export default class ElectionModel {
     ) {
       let result = lastElection.getResults(pdID);
       const valid = result.summary.valid;
-      const partyToPVotesSorted = normPDTopartyToPVotesSorted[pdID];
+      const partyToPVotes = normPDToPartyToPVotes[pdID];
       const partyToVotes = Object.fromEntries(
-        Object.entries(partyToPVotesSorted).map(function ([partyID, voteInfo]) {
-          return [partyID, Math.round(voteInfo.pVotesPredicted * valid)];
+        Object.entries(partyToPVotes).map(function ([partyID, pVotes]) {
+          return [partyID, Math.round(pVotes * valid)];
         })
       );
       result.partyToVotes = new PartyToVotes(partyToVotes);

ebab5ad5fd2dce985636954a92756a44d2fa0314
nuuuwan <nuuuwan@gmail.com>
2024-08-11 20:57:39 +0530

    lint only (jli)

---
+++ src/view/molecules/FinalOutcomeView.js
@@ -0,0 +1,9 @@
+import { Box, Typography } from "@mui/material";
+
+export default function FinalOutcomeView({result}) {
+  return (
+    <Box>
+      <Typography variant="h6">FinalOutcomeView</resultTypography>
+    </Box>
+  );
+}

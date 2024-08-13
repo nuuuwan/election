f396e141a13dd56c80a585898068b0250bafd0bd
nuuuwan <nuuuwan@gmail.com>
2024-08-13 20:36:36 +0530

    unused

--- src/view/molecules/PredictionView.js
+++ src/view/molecules/PredictionView.js
@@ -1,4 +1,4 @@
-import { CircularProgress, Stack } from "@mui/material";
+import { Stack } from "@mui/material";
 import { ElectionModel } from "../../nonview/core";
 import { FinalOutcomeView, ResultSingleView } from ".";
 

98e6efd159cb5bf8a239c5e5433332f1073826ff
nuuuwan <nuuuwan@gmail.com>
2024-08-13 14:57:42 +0530

    HexMap bounds

--- src/view/molecules/HexagonMap/HexagonMap.js
+++ src/view/molecules/HexagonMap/HexagonMap.js
@@ -225,7 +225,7 @@ export default function HexagonMap({ resultsIdx, pdIdx }) {
     <svg
       width={"100%"}
       height={"100vh"}
-      viewBox="0 -4 11 35"
+      viewBox="0 -6 11 40"
       fontFamily={STYLE.FONT_FAMILY}
     >
       <SVGTitles />

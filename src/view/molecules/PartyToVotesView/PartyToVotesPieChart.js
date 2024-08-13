d3deec76bdbef5e9af0c24e012e87774b3b34de8
nuuuwan <nuuuwan@gmail.com>
2024-08-13 11:54:18 +0530

    Fixed PieChart centering BUG

--- src/view/molecules/PartyToVotesView/PartyToVotesPieChart.js
+++ src/view/molecules/PartyToVotesView/PartyToVotesPieChart.js
@@ -3,6 +3,7 @@ import { Party } from "../../../nonview/core";
 import { Format } from "../../../nonview/base";
 
 import { STYLE } from "../../../nonview/constants";
+import { Box } from "@mui/material";
 
 export default function PartyToVotesPieChart({ partyToVotes }) {
   const data = Object.entries(partyToVotes.partyToVotesSortedOthered).map(
@@ -18,25 +19,27 @@ export default function PartyToVotesPieChart({ partyToVotes }) {
   );
 
   return (
-    <PieChart
-      series={[
-        {
-          data,
-          arcLabel: (item) => Format.percentVotes(item.value),
-          arcLabelMinAngle: 60,
-        },
-      ]}
-      sx={{
-        [`& .${pieArcLabelClasses.root}`]: {
-          fill: "white",
-          fontSize: "200%",
-          fontFamily: STYLE.FONT_FAMILY,
-        },
-      }}
-      slotProps={{ legend: { hidden: true } }}
-      width={400}
-      height={400}
-      margin={{ top: 32, right: 0, bottom: 32, left: 0 }}
-    />
+    <Box sx={{ display: "flex", p: 1 }}>
+      <PieChart
+        series={[
+          {
+            data,
+            arcLabel: (item) => Format.percentVotes(item.value),
+            arcLabelMinAngle: 60,
+          },
+        ]}
+        sx={{
+          [`& .${pieArcLabelClasses.root}`]: {
+            fill: "white",
+            fontSize: "200%",
+            fontFamily: STYLE.FONT_FAMILY,
+          },
+        }}
+        slotProps={{ legend: { hidden: true } }}
+        width={320}
+        height={320}
+        margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
+      />
+    </Box>
   );
 }

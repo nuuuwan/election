39a072ab32131ae746cd0dbab8a973ccaad645fa
nuuuwan <nuuuwan@gmail.com>
2024-08-13 14:15:07 +0530

    unused

--- src/view/molecules/PDSelector.js
+++ src/view/molecules/PDSelector.js
@@ -1,4 +1,4 @@
-import { Box, Stack, Typography } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 import { Party } from "../../nonview/core";
 import { CustomSelect } from "../atoms";
 

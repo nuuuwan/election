e960bfe8e5ec2a2ca5015c221e8670b982e3f536
nuuuwan <nuuuwan@gmail.com>
2024-08-14 10:02:29 +0530

    lint only (jli)

--- src/nonview/base/Format/FormatInt.js
+++ src/nonview/base/Format/FormatInt.js
@@ -15,20 +15,20 @@ let FormatInt = {
     }
     // M
     if (x > 10_000_000) {
-      return `${(x / 1_000_000).toFixed(1)}M`;
+      return `${(x / 1_000_000).toFixed(0)}M`;
     }
     if (x > 1_000_000) {
-      return `${(x / 1_000_000).toFixed(2)}M`;
+      return `${(x / 1_000_000).toFixed(1)}M`;
     }
     // K
     if (x > 100_000) {
-      return `${(x / 1_000).toFixed(0)}K`;
+      return `${10 * (x / 10_000).toFixed(0)}K`;
     }
     if (x > 10_000) {
-      return `${(x / 1_000).toFixed(1)}K`;
+      return `${(x / 1_000).toFixed(0)}K`;
     }
     if (x > 1_000) {
-      return `${(x / 1_000).toFixed(2)}K`;
+      return `${(x / 1_000).toFixed(1)}K`;
     }
     return x.toLocaleString();
   },

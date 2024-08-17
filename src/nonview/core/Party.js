d76a1b621db31d5d27909452f65b9dbf4e91f510
nuuuwan <nuuuwan@gmail.com>
2024-08-17 18:24:21 +0530

    lint

--- src/nonview/core/Party.js
+++ src/nonview/core/Party.js
@@ -4,10 +4,9 @@ export default class Party {
   static OTHER = new Party("Other", "Other");
   static UNCERTAIN = new Party("Uncertain", "Uncertain");
   static NON_PARTY_ID_LIST = [Party.OTHER.id, Party.UNCERTAIN.id];
-  
+
   constructor(id) {
     this.id = id;
-
   }
 
   static fromID(id) {
@@ -24,8 +23,7 @@ export default class Party {
     return POLITICAL_PARTY_TO_COLOR[this.id] || STYLE.COLOR.LIGHT;
   }
 
-  get isNonParty () {
+  get isNonParty() {
     return Party.NON_PARTY_ID_LIST.includes(this.id);
   }
-
 }
--- src/view/atoms/PartyView.js
+++ src/view/atoms/PartyView.js
@@ -2,7 +2,7 @@ import { Typography } from "@mui/material";
 import { Party } from "../../nonview/core";
 import { Color } from "../../nonview/base";
 
-const STYLE_LABEL ={
+const STYLE_LABEL = {
   padding: 0.25,
   margin: 0.25,
   borderRadius: 1,
@@ -12,21 +12,21 @@ const STYLE_LABEL ={
 export default function PartyView({ partyID, pVotes }) {
   const party = Party.fromID(partyID);
 
-  const label = (partyID === Party.UNCERTAIN.id) ? "Margin of Error" : partyID;
+  const label = partyID === Party.UNCERTAIN.id ? "Margin of Error" : partyID;
   let backgroundColor = party.isNonParty ? party.color : "white";
   let textColor = "white";
   if (pVotes) {
     const opacity = Color.getOpacity(pVotes);
-    textColor = party.isNonParty ? party.color : Color.getTextColor(backgroundColor, opacity);
+    textColor = party.isNonParty
+      ? party.color
+      : Color.getTextColor(backgroundColor, opacity);
     backgroundColor += Color.getOpacityChar(pVotes);
   }
 
   return (
     <Typography
       variant="caption"
-      sx={Object.assign({}, STYLE_LABEL, {backgroundColor,
-        color: textColor,
-      })}
+      sx={Object.assign({}, STYLE_LABEL, { backgroundColor, color: textColor })}
       component="span"
     >
       {label}

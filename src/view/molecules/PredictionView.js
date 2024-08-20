c051e41ba239cdfda723f4714111ea09cd94c6c3
nuuuwan <nuuuwan@gmail.com>
2024-08-20 18:53:18 +0530

    lint only

--- src/view/molecules/PredictionView.js
+++ src/view/molecules/PredictionView.js
@@ -1,14 +1,11 @@
-
 import { ResultSingleView } from ".";
 
 export default function PredictionView({ projectedElection, db }) {
   return (
-
-      <ResultSingleView
-        election={projectedElection}
-        entID="LK"
-        elections={db.elections}
-      />
-
+    <ResultSingleView
+      election={projectedElection}
+      entID="LK"
+      elections={db.elections}
+    />
   );
 }

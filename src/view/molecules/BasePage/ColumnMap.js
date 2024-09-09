37966ff017b2e2d1f12aee106978065d1cdb23c4
nuuuwan <nuuuwan@gmail.com>
2024-09-09 17:45:13 +0530

    HexMapView BUG

--- src/view/molecules/BasePage/ColumnMap.js
+++ src/view/molecules/BasePage/ColumnMap.js
@@ -1,6 +1,6 @@
 import { Box, Typography } from "@mui/material";
 
-import { HexMap } from "..";
+import HexMapView from "../../../view/molecules/HexMapView/HexMapView";
 
 import CustomStack from "./CustomStack";
 import { Translate } from "../../../nonview/base";
@@ -22,7 +22,7 @@ export default function ColumnMap() {
         <Typography variant="body1">{subTitleProgress}</Typography>
         <Typography variant="h4">{Translate("Maps")}</Typography>
       </Box>
-      <HexMap />
+      <HexMapView />
     </CustomStack>
   );
 }

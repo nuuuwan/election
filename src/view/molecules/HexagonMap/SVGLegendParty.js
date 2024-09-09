acb6770d1612eb22213e01ee9560fd87bccb1727
nuuuwan <nuuuwan@gmail.com>
2024-09-09 14:38:31 +0530

    Fix Labels on Hexagons

--- src/view/molecules/HexagonMap/SVGLegendParty.js
+++ src/view/molecules/HexagonMap/SVGLegendParty.js
@@ -3,6 +3,7 @@ import { DataContext, Party } from "../../../nonview/core";
 
 import SVGHexagon from "./SVGHexagon";
 import StyleHexagonMap from "./StyleHexagonMap";
+import SVGHexagonLabel from "./SVGHexagonLabel";
 
 export default function SVGLegendParty({ x, y }) {
   const data = useContext(DataContext);
@@ -18,20 +19,39 @@ export default function SVGLegendParty({ x, y }) {
     })
     .map(function ([partyID, nWins], i) {
       const party = Party.fromID(partyID);
+
+      const [x1 ,y1] = [
+        x + parseInt(i / StyleHexagonMap.N_COLS),
+        y +
+        ((i % StyleHexagonMap.N_COLS) +
+          (parseInt(i / StyleHexagonMap.N_COLS) % 2 === 1 ? 0.5 : 0)) /
+          Math.cos(Math.PI / 6),
+      ]
+        
       return (
+        <g  key={partyID}>
         <SVGHexagon
-          key={partyID}
-          x={x + parseInt(i / StyleHexagonMap.N_COLS)}
+         
+          x={x1}
           y={
-            y +
-            ((i % StyleHexagonMap.N_COLS) +
-              (parseInt(i / StyleHexagonMap.N_COLS) % 2 === 1 ? 0.5 : 0)) /
-              Math.cos(Math.PI / 6)
+            y1
           }
           color={party.color}
           label={partyID}
           opacity={1}
         />
+      <SVGHexagonLabel 
+
+x={x1}
+y={
+  y1
+}
+        color={party.color}
+        label={partyID}
+        opacity={1}
+      />
+
+        </g>
       );
     });
 }
--- src/view/molecules/HexagonMap/SVGLegendPercentages.js
+++ src/view/molecules/HexagonMap/SVGLegendPercentages.js
@@ -2,23 +2,38 @@ import { STYLE } from "../../../nonview/constants";
 import { Color, Format } from "../../../nonview/base";
 
 import SVGHexagon from "./SVGHexagon";
+import SVGHexagonLabel from "./SVGHexagonLabel";
 
 export default function SVGLegendPercentages({ x, y }) {
   const N_COLS = 2;
+  
   return [0.25, 0.5, 0.75].map(function (p, i) {
+    const [x1, y1] = [
+      x + parseInt(i / N_COLS),
+      y +
+        ((i % N_COLS) + (parseInt(i / N_COLS) % 2 === 1 ? 0.5 : 0)) /
+          Math.cos(Math.PI / 6),
+    ]
+  
     return (
+      <g key={i}>
       <SVGHexagon
         key={i}
-        x={x + parseInt(i / N_COLS)}
-        y={
-          y +
-          ((i % N_COLS) + (parseInt(i / N_COLS) % 2 === 1 ? 0.5 : 0)) /
-            Math.cos(Math.PI / 6)
-        }
+        x={x1}
+        y={y1}
         color={STYLE.COLOR.LIGHT}
         label={Format.percent(p)}
         opacity={Color.getOpacity(p)}
       />
+      <SVGHexagonLabel 
+        x={x1}
+        y={y1}
+        color={STYLE.COLOR.DARK}
+        label={Format.percent(p)}
+        opacity={1} 
+      />
+      </g>
+
     );
   });
 }

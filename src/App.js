bdd418fb2d0a715416b54f662e8fd11b932688c9
nuuuwan <nuuuwan@gmail.com>
2024-09-09 14:52:50 +0530

    Added THEME

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React, { Component } from "react";
 
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material/styles";
 
 import BasePage from "./view/pages/BasePage/BasePage";
 
-import { STYLE } from "./nonview/constants";
+import {THEME} from "./view/_constants";
 
 import "./App.css";
-
-const THEME = createTheme({
-  typography: {
-    fontFamily: STYLE.FONT_FAMILY,
-    fontSize: STYLE.FONT_SIZE,
-  },
-});
 
 export default class App extends Component {
   render() {
---
+++ src/view/_constants/THEME.js
@@ -0,0 +1,10 @@
+import { createTheme } from "@mui/material";
+
+const THEME = createTheme({
+    typography: {
+      fontFamily: "Cairo",
+      fontSize: 13,
+    },
+  });
+
+export default THEME;

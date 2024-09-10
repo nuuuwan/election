f3e284c325b40fd317dddfd4f3cf015ad5b3a40d
nuuuwan <nuuuwan@gmail.com>
2024-09-10 15:59:08 +0530

    Simplify SVGLegendParty

--- src/view/molecules/HexMapView/SVGLegendParty.js
+++ src/view/molecules/HexMapView/SVGLegendParty.js
@@ -11,40 +11,27 @@ export default function SVGLegendParty({ x, y }) {
     return null;
   }
   const { election } = data;
-  const partyToWins = election.getPartyToWins();
+  return Object.keys(election.getPartyToWins()).map(function (partyID, i) {
+    const color = Party.fromID(partyID).color;
+    const [x1, y1] = [
+      x + parseInt(i / StyleHexMap.N_COLS),
+      y +
+        ((i % StyleHexMap.N_COLS) +
+          (parseInt(i / StyleHexMap.N_COLS) % 2 === 1 ? 0.5 : 0)) /
+          Math.cos(Math.PI / 6),
+    ];
 
-  return Object.entries(partyToWins)
-    .sort(function ([partyID1, nWins1], [partyID2, nWins2]) {
-      return nWins2 - nWins1;
-    })
-    .map(function ([partyID, nWins], i) {
-      const party = Party.fromID(partyID);
-
-      const [x1, y1] = [
-        x + parseInt(i / StyleHexMap.N_COLS),
-        y +
-          ((i % StyleHexMap.N_COLS) +
-            (parseInt(i / StyleHexMap.N_COLS) % 2 === 1 ? 0.5 : 0)) /
-            Math.cos(Math.PI / 6),
-      ];
-
-      return (
-        <g key={partyID}>
-          <SVGHexPolygon
-            x={x1}
-            y={y1}
-            color={party.color}
-            label={partyID}
-            opacity={1}
-          />
-          <SVGHexText
-            x={x1}
-            y={y1}
-            color={party.color}
-            label={partyID}
-            opacity={1}
-          />
-        </g>
-      );
-    });
+    return (
+      <g key={partyID}>
+        <SVGHexPolygon
+          x={x1}
+          y={y1}
+          color={color}
+          label={partyID}
+          opacity={1}
+        />
+        <SVGHexText x={x1} y={y1} color={color} label={partyID} opacity={1} />
+      </g>
+    );
+  });
 }

56da6e3d970a47c85ea15f45b4872fa45271b41f
nuuuwan <nuuuwan@gmail.com>
2024-09-11 10:27:30 +0530

    Simplify PartyView - Part 1

--- src/view/atoms/PartyView.js
+++ src/view/atoms/PartyView.js
@@ -1,7 +1,7 @@
 import { Typography } from "@mui/material";
 import { Party } from "../../nonview/core";
-import { Color, Translate } from "../../nonview/base";
-import { useDataContext } from "../../nonview/core/DataProvider";
+import {  Translate } from "../../nonview/base";
+
 
 const STYLE_LABEL = {
   padding: 0.25,
@@ -11,48 +11,18 @@ const STYLE_LABEL = {
   textAlign: "center",
 };
 
-function getLabel(partyID, election) {
-  if (partyID === Party.UNCERTAIN.id) {
-    return "Error Margin";
-  }
-  if (partyID === Party.OTHER.id) {
-    return "Other";
-  }
-  return partyID;
-}
 
-function getStyle(party, pVotes) {
-  let backgroundColor = party.isNonParty ? "white" : party.color;
-  let textColor = party.isNonParty ? party.color : "white";
 
-  if (pVotes) {
-    const opacity = Color.getOpacity(pVotes);
-    textColor = party.isNonParty
-      ? party.color
-      : Color.getTextColor(backgroundColor, opacity);
-    backgroundColor += Color.getOpacityChar(pVotes);
-  }
-
-  return { backgroundColor, textColor };
-}
-
-export default function PartyView({ partyID, pVotes }) {
-  const data = useDataContext();
-  if (!data) {
-    return null;
-  }
-  const { election } = data;
+export default function PartyView({ partyID, sx }) {
   const party = Party.fromID(partyID);
-  const label = getLabel(partyID, election);
-  const { backgroundColor, textColor } = getStyle(party, pVotes);
-
-  const translatedLabel = Translate(label) || "";
+  
+  const translatedLabel = Translate(partyID) || "";
   const shortLabel = translatedLabel.toUpperCase();
 
   return (
     <Typography
       variant="caption"
-      sx={Object.assign({}, STYLE_LABEL, { backgroundColor, color: textColor })}
+      sx={Object.assign({}, STYLE_LABEL, { backgroundColor: party.color, color: "white" }, sx)}
       component="span"
     >
       {shortLabel}

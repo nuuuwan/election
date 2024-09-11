afe3217fda0f715e01adfa914966eda416f892dd
nuuuwan <nuuuwan@gmail.com>
2024-09-11 10:05:46 +0530

    Removed unused Format

--- src/nonview/base/Format/Format.js
+++ src/nonview/base/Format/Format.js
@@ -1,19 +1,16 @@
 import FormatGeneric from "./FormatGeneric";
 import FormatInt from "./FormatInt";
 import FormatPercent from "./FormatPercent";
-import FormatPercentagePoint from "./FormatPercentagePoint";
-import FormatString from "./FormatString";
-import FormatMisc from "./FormatMisc";
 class Format {}
 
 Object.assign(
   Format,
   FormatGeneric,
-  FormatMisc,
-  FormatString,
+
+
   FormatInt,
   FormatPercent,
-  FormatPercentagePoint
+
 );
 
 export default Format;

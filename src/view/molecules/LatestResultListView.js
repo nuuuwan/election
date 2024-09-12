8e592d9ec945e4755e66738b11b43b89e2f970df
nuuuwan <nuuuwan@gmail.com>
2024-09-12 17:51:01 +0530

    CustomPagination

--- src/view/molecules/LatestResultListView.js
+++ src/view/molecules/LatestResultListView.js
@@ -1,13 +1,14 @@
-import { Pagination, Stack, Typography } from "@mui/material";
+import {  Stack, Typography } from "@mui/material";
 import { useDataContext } from "../../nonview/core/DataProvider";
 import CumResultsView from "./CumResultsView";
-import { useBasePageHandlerContext } from "../pages/BasePage/BasePageHandlerProvider";
+
+import { CustomPagination } from "../atoms";
 
 const N_DISPLAY = 3;
 
 export default function LatestResultListView() {
   const data = useDataContext();
-  const handlers = useBasePageHandlerContext();
+
 
   if (!data) {
     return null;
@@ -19,23 +20,14 @@ export default function LatestResultListView() {
     .slice(Math.max(0, nResultsDisplay - N_DISPLAY), nResultsDisplay)
     .reverse();
 
-  const { setNResultsDisplay } = handlers;
-  const onChange = function (event, value) {
-    setNResultsDisplay(value);
-  };
+
+
 
   return (
     <Stack direction="column" alignItems="center">
       <Typography variant="h4">Results</Typography>
 
-      <Pagination
-        key={nResultsDisplay}
-        count={n}
-        defaultPage={nResultsDisplay}
-        siblingCount={1}
-        boundaryCount={1}
-        onChange={onChange}
-      />
+      <CustomPagination />
 
       {resultListDisplay.map(function (result) {
         return <CumResultsView key={result.entID} entID={result.entID} />;

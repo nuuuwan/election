0aafd9e4d626733d52302de818c23565560dee0e
nuuuwan <nuuuwan@gmail.com>
2024-09-15 19:59:36 +0530

    width={180}

--- src/view/molecules/ResultBarChart.js
+++ src/view/molecules/ResultBarChart.js
@@ -69,7 +69,7 @@ export default function ResultBarChart({ resultsElection, entID }) {
         barLabel={getBarLabel()}
         layout="horizontal"
         bottomAxis={null}
-        width={200}
+        width={180}
         height={50}
         sx={getStyle()}
         slotProps={{ legend: { hidden: true } }}

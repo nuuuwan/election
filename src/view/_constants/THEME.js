a4fe2378b0f5b467f08ed30fc17085bc86a8cb7f
nuuuwan <nuuuwan@gmail.com>
2024-09-15 23:07:30 +0530

    Alata

--- src/view/_constants/THEME.js
+++ src/view/_constants/THEME.js
@@ -4,7 +4,7 @@ import CustomURLContext from "../../nonview/core/CustomURLContext";
 function getThemeData() {
   const lang = CustomURLContext.get().lang;
 
-  let fontFamily = "ABeeZee";
+  let fontFamily = "Alata";
   let fontSize = 12;
   if (lang === "si") {
     fontFamily = "Noto Sans Sinh.";

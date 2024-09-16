273ffae5dfaddd12149e127bbe0e16a46d3faaae
nuuuwan <nuuuwan@gmail.com>
2024-09-16 23:07:23 +0530

    various

--- src/view/atoms/NoResultsAlert.js
+++ src/view/atoms/NoResultsAlert.js
@@ -4,7 +4,7 @@ import { Translate } from "../../nonview/base";
 export default function NoResultsAlert() {
   return (
     <Box sx={{ paddingTop: 10, paddingBottom: 20, height: "90vh" }}>
-      <Typography variant="h2">{Translate("Await results")}...</Typography>
+      <Typography variant="h1">{Translate("Await results")}...</Typography>
     </Box>
   );
 }

322bcd7b6f57537aa2902bd0599bc2a425fa1c34
nuuuwan <nuuuwan@gmail.com>
2024-09-16 10:27:11 +0530

    Simplify CumResultsView

--- src/view/molecules/CumResultsView.js
+++ src/view/molecules/CumResultsView.js
@@ -7,27 +7,9 @@ import { ResultBarChart, HistoryView, SummaryView } from ".";
 import PartyToVotesStatsView from "../../view/molecules/PartyToVotesStatsView";
 import { Color } from "../../nonview/base";
 
-export default function CumResultsView({ entID, direction = "column" }) {
-  const data = useDataContext();
-  if (!data) {
-    return null;
-  }
-  const { electionDisplay, activePDID } = data;
 
-  const isActiveEnt = activePDID === entID;
-
-  const result = electionDisplay.resultIdx[entID];
-  if (!result) {
-    return null;
-  }
-
-  const backgroundColor = isActiveEnt
-    ? Color.getColorWithAlpha(result.color, 0.05)
-    : "white";
-
-  const border = isActiveEnt ? "5px solid " + result.color : "";
-
-  const contentList = [
+function getContentList({entID, result, electionDisplay}) {
+  return [
     <EntView
       entID={entID}
       useLongName={true}
@@ -40,30 +22,58 @@ export default function CumResultsView({ entID, direction = "column" }) {
     <PartyToVotesStatsView partyToVotes={result.partyToVotes} />,
     <ResultsReleasedView entID={entID} />,
     <HistoryView entID={entID} />,
-  ];
+  ]
+}
 
-  if (direction === "column") {
-    return (
-      <Box sx={{ p: 1, backgroundColor, border, borderRadius: 6 }}>
-        {contentList.map(function (content, i) {
-          return (
-            <Box
-              key={i}
-              sx={{ p: 0.3, m: 0.15 }}
-              justifyContent="center"
-              alignItems="center"
-              alignContent="center"
-              display="flex"
-            >
-              {content}
-            </Box>
-          );
-        })}
-      </Box>
-    );
+function CumResultsColumnView({entID, }) {
+
+  const data = useDataContext();
+  if (!data) {
+    return null;
   }
+  const { electionDisplay, activePDID } = data;
+  const result = electionDisplay.resultIdx[entID];
+  const contentList = getContentList({entID, result, electionDisplay});
+
+  const isActiveEnt = activePDID === entID;
+  const backgroundColor = isActiveEnt
+    ? Color.getColorWithAlpha(result.color, 0.05)
+    : "white";
+
+  const border = isActiveEnt ? "5px solid " + result.color : "";
+
+
   return (
-    <tr>
+    <Box sx={{ p: 1, backgroundColor, border, borderRadius: 6 }}>
+      {contentList.map(function (content, i) {
+        return (
+          <Box
+            key={i}
+            sx={{ p: 0.3, m: 0.15 }}
+            justifyContent="center"
+            alignItems="center"
+            alignContent="center"
+            display="flex"
+          >
+            {content}
+          </Box>
+        );
+      })}
+    </Box>
+  );
+}
+
+function CumResultsViewTableRowView({entID}) {
+  const data = useDataContext();
+  if (!data) {
+    return null;
+  }
+  const { electionDisplay } = data;
+  const result = electionDisplay.resultIdx[entID];
+  const contentList = getContentList({entID, result, electionDisplay});
+
+return (
+  <tr>
       {contentList.map(function (content, i) {
         return (
           <td key={i} style={{ padding: 12, borderBottom: "1px solid #eee" }}>
@@ -72,5 +82,15 @@ export default function CumResultsView({ entID, direction = "column" }) {
         );
       })}
     </tr>
+)
+}
+
+
+export default function CumResultsView({ entID, direction = "column" }) {
+  if (direction === "column") {
+    return <CumResultsColumnView entID={entID}  />;
+  }
+  return (
+    <CumResultsViewTableRowView entID={entID} />
   );
 }

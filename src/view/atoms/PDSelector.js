1098a1847a313091c7424ae646958ea2898ec580
nuuuwan <nuuuwan@gmail.com>
2024-09-16 10:18:38 +0530

    Simplify PDSelector

--- src/view/atoms/PDSelector.js
+++ src/view/atoms/PDSelector.js
@@ -4,7 +4,7 @@ import { Translate } from "../../nonview/base";
 import { useDataContext } from "../../nonview/core/DataProvider";
 import { useBasePageHandlerContext } from "../../view/pages/BasePage/BasePageHandlerProvider";
 
-function buildRenderMenuItemInner(resultIdx, edIdx, variant1, variant2) {
+function buildRenderMenuItemInner(resultIdx, edIdx, ) {
   const renderMenuItemInner = function (pd, i) {
     const result = resultIdx[pd.id];
     if (!result) {
@@ -20,10 +20,10 @@ function buildRenderMenuItemInner(resultIdx, edIdx, variant1, variant2) {
 
     return (
       <Stack direction="row" gap={0.5} alignItems="center">
-        <Typography variant={variant1} color={colorPD}>
+        <Typography variant={"h6"} color={colorPD}>
           {Translate(pd.name)}
         </Typography>
-        <Typography variant={variant2} color={colorED} sx={{ opacity: 0.5 }}>
+        <Typography variant={"body1"} color={colorED} sx={{ opacity: 0.5 }}>
           {Translate(ed.name)}
         </Typography>
       </Stack>
@@ -31,6 +31,7 @@ function buildRenderMenuItemInner(resultIdx, edIdx, variant1, variant2) {
   };
   return renderMenuItemInner;
 }
+
 export default function PDSelector() {
   const { setActivePDID } = useBasePageHandlerContext();
   const data = useDataContext();
@@ -45,30 +46,17 @@ export default function PDSelector() {
         return electionDisplay.resultIdx[pd.id];
       })}
       value={pdIdx[activePDID]}
-      getID={function (pd) {
-        if (!pd) {
-          return null;
-        }
-        return pd.name;
-      }}
-      onChange={function (pd) {
-        setActivePDID(pd.id);
-      }}
+      getID={(pd) => pd?.name}
+      onChange={ (pd) => setActivePDID(pd.id)}
       renderValue={buildRenderMenuItemInner(
         electionDisplay.resultIdx,
         edIdx,
-        "h6",
-        "body1"
       )}
       renderMenuItemInner={buildRenderMenuItemInner(
         electionDisplay.resultIdx,
         edIdx,
-        "body1",
-        "body2"
       )}
-      getDividerKey={function (pd) {
-        return pd.name.substring(0, 1);
-      }}
+      getDividerKey={(pd) =>  pd.name.substring(0, 1)}
     />
   );
 }

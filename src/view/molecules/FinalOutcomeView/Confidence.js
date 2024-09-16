83ae7671fb12cb40e79f5e8176ef718d4079d7d1
nuuuwan <nuuuwan@gmail.com>
2024-09-16 15:13:27 +0530

    HIde 90% when complete

--- src/view/molecules/FinalOutcomeView/Confidence.js
+++ src/view/molecules/FinalOutcomeView/Confidence.js
@@ -1,8 +1,23 @@
 import { Typography } from "@mui/material";
 import { Format, Translate } from "../../../nonview/base";
 import { FinalOutcome } from "../../../nonview/core";
+import { useDataContext } from "../../../nonview/core/DataProvider";
 
 export default function Confidence() {
+  const data = useDataContext();
+  if (!data) {
+    return null;
+  }
+  const { electionDisplay, pdIdx, electionPrevious } = data;
+
+  const { nResultsTotal, nResultsReleased } =
+    electionDisplay.getReleaseStats("LK", pdIdx, electionPrevious);
+
+  const isComplete = nResultsReleased === nResultsTotal;  
+  if (isComplete) {
+    return null;
+  }
+
   return (
     <Typography variant="caption">
       &gt;{Format.percent(FinalOutcome.P_BASE)} {Translate("Confidence")}

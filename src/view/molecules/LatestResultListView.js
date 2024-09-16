bda04b063693a97df492e629f8ed7b677c651e57
nuuuwan <nuuuwan@gmail.com>
2024-09-16 10:44:38 +0530

    Simplify LatestResultListView

--- src/view/molecules/LatestResultListView.js
+++ src/view/molecules/LatestResultListView.js
@@ -4,17 +4,19 @@ import CumResultsView from "./CumResultsView";
 import { ProvinceUtils } from "../../nonview/base";
 import { PDSelector } from "../atoms";
 
+
+
+
 export default function LatestResultListView() {
   const data = useDataContext();
-
   if (!data) {
     return null;
   }
   const { pdIdx, electionDisplay, activePDID } = data;
-  const resultPD = electionDisplay.resultIdx[activePDID];
-  const pdEnt = pdIdx[resultPD.entID];
-
+  const pdEnt = pdIdx[activePDID];
   const resultIdx = electionDisplay.resultIdx;
+
+  const resultPD = electionDisplay.resultIdx[activePDID];
   const resultED = resultIdx[pdEnt.d.ed_id];
   const resultProvince = resultIdx[ProvinceUtils.getProvinceIDForPDEnt(pdEnt)];
   const resultLK = resultIdx["LK"];
@@ -22,22 +24,10 @@ export default function LatestResultListView() {
   return (
     <Stack direction="column" sx={{ color: resultPD.color }}>
       <PDSelector />
-
-      <Grid2 container spacing={2} rowSpacing={3} justifyContent="center">
+      <Grid2 container spacing={2} justifyContent="center">
         {[resultPD, resultED, resultProvince, resultLK].map(function (result) {
-          if (!result) {
-            return null;
-          }
           return (
-            <Grid2
-              xs={12}
-              md={12}
-              xl={12}
-              key={result.entID}
-              justifyContent="center"
-              alignContent="center"
-              alignItems="center"
-            >
+            <Grid2 key={result.entID}>
               <CumResultsView entID={result.entID} />
             </Grid2>
           );

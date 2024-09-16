3b35ad02168ec7728983f47de3ccfdc3f63504c9
nuuuwan <nuuuwan@gmail.com>
2024-09-16 22:26:21 +0530

    style

--- src/view/atoms/CustomAlert.js
+++ src/view/atoms/CustomAlert.js
@@ -4,7 +4,7 @@ export default function CustomAlert({ children }) {
   return (
     <Alert
       severity="info"
-      sx={{ p: 2, textAlign: "justify", m: 3, borderRadius: 3 }}
+      sx={{ p: 2, textAlign: "justify", m: 3, marginTop: 6, borderRadius: 3 }}
     >
       {children}
     </Alert>

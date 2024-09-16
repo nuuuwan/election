b66dc2aa5fd3340c72d3420033087856d290ebe2
nuuuwan <nuuuwan@gmail.com>
2024-09-16 23:03:59 +0530

    NoResultsAlert

--- src/view/atoms/NoResultsAlert.js
+++ src/view/atoms/NoResultsAlert.js
@@ -1,18 +1,10 @@
 import { Box, Typography } from "@mui/material";
 import { Translate } from "../../nonview/base";
-import { useDataContext } from "../../nonview/core/DataProvider";
 
 export default function NoResultsAlert() {
-  const data = useDataContext();
-  if (!data) {
-    return null;
-  }
-  const { election } = data;
-
   return (
     <Box sx={{ paddingTop: 10, paddingBottom: 20, height: "90vh" }}>
-      <Typography variant="h2">{election.titleLong}</Typography>
-      <Typography variant="h5">{Translate("Await results")}...</Typography>
+      <Typography variant="h2">{Translate("Await results")}...</Typography>
     </Box>
   );
 }

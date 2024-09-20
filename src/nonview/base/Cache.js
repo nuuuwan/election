485f0a87fb1ece3751b36ef69f010c381ae0a8c3
nuuuwan <nuuuwan@gmail.com>
2024-09-20 14:10:17 +0530

    isQuotaExceededError

--- src/nonview/base/Cache.js
+++ src/nonview/base/Cache.js
@@ -1,3 +1,20 @@
+function isQuotaExceededError(error) {
+  return (
+    error instanceof DOMException &&
+    (
+      // Everything except Firefox
+      error.code === 22 ||
+      // Firefox
+      error.code === 1014 ||
+      // Test name field too, because code might not be present
+      // Everything except Firefox
+      error.name === "QuotaExceededError" ||
+      // Firefox
+      error.name === "NS_ERROR_DOM_QUOTA_REACHED"
+    )
+  );
+}
+
 export default class Cache {
   static clear() {
     localStorage.clear();
@@ -14,15 +31,19 @@ export default class Cache {
     try {
       const coldItemJSON = JSON.stringify(coldItem);
       localStorage.setItem(cacheKey, coldItemJSON);
-
+      
       // logging
       const size = coldItemJSON.length;
       const sizeK = Math.round(size / 1000);
       const logger = sizeK > 500 ? console.warn : console.log;
       logger(`📦 "${cacheKey}" (${sizeK}KB)`);
+
     } catch (e) {
       console.error(e);
-      Cache.clear();
+      if (isQuotaExceededError(e)) {
+        Cache.clear();
+      }
+      
     }
     return coldItem;
   }

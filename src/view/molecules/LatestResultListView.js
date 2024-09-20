be4da4c096067467a07d9ecd08999226124f4db4
nuuuwan <nuuuwan@gmail.com>
2024-09-20 09:14:25 +0530

    LatestResultListView <= 40

--- src/view/molecules/LatestResultListView.js
+++ src/view/molecules/LatestResultListView.js
@@ -3,35 +3,42 @@ import { useDataContext } from "../../nonview/core/DataProvider";
 import { CumResultsColumnView } from "./CumResultsView";
 import { EntType, ProvinceUtils } from "../../nonview/base";
 
+
+function getResultList({ allRegionIdx, electionDisplay, activeEntID }) {
+  const resultIdx = electionDisplay.resultIdx;
+
+  const ent = allRegionIdx[activeEntID];
+
+  let resultList;
+  const resultLK = resultIdx["LK"];
+  if (electionDisplay.baseEntType === EntType.PD) {
+    const resultPD = electionDisplay.resultIdx[activeEntID];
+    const resultED = resultIdx[ent.d.ed_id];
+    const resultProvince = resultIdx[ProvinceUtils.getProvinceIDForPDEnt(ent)];
+    resultList = [resultPD, resultED, resultProvince, resultLK];
+
+  } else if (electionDisplay.baseEntType === EntType.ED) {
+    const resultED = resultIdx[activeEntID];
+    const resultProvince =
+      resultIdx[ProvinceUtils.getProvinceIDForEDID(activeEntID)];
+    resultList = [resultED, resultProvince, resultLK];
+
+  }
+
+  return resultList;
+}
+
 export default function LatestResultListView() {
   const data = useDataContext();
   if (!data) {
     return null;
   }
   const { allRegionIdx, electionDisplay, activeEntID } = data;
-  const resultIdx = electionDisplay.resultIdx;
 
-  const ent = allRegionIdx[activeEntID];
-
-  let resultList, resultBaseEnt;
-  const resultLK = resultIdx["LK"];
-
-  if (electionDisplay.baseEntType === EntType.PD) {
-    const resultPD = electionDisplay.resultIdx[activeEntID];
-    const resultED = resultIdx[ent.d.ed_id];
-    const resultProvince = resultIdx[ProvinceUtils.getProvinceIDForPDEnt(ent)];
-    resultList = [resultPD, resultED, resultProvince, resultLK];
-    resultBaseEnt = resultPD;
-  } else if (electionDisplay.baseEntType === EntType.ED) {
-    const resultED = resultIdx[activeEntID];
-    const resultProvince =
-      resultIdx[ProvinceUtils.getProvinceIDForEDID(activeEntID)];
-    resultList = [resultED, resultProvince, resultLK];
-    resultBaseEnt = resultED;
-  }
+  const resultList = getResultList({ allRegionIdx, electionDisplay, activeEntID });
 
   return (
-    <Stack direction="column" sx={{ color: resultBaseEnt.color }}>
+    <Stack direction="column" sx={{ color: resultList[0].color }}>
       <Grid2 container justifyContent="center">
         {resultList.map(function (result) {
           return (

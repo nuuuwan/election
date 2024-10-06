1975634dbd3bf1f49f8fe493be45b106609698a8
nuuuwan <nuuuwan@gmail.com>
2024-10-06 17:39:57 +0530

    Lock Header

--- src/view/pages/BasePage/PageHeader.js
+++ src/view/pages/BasePage/PageHeader.js
@@ -6,7 +6,7 @@ import { useDataSlowContext } from "../../../nonview/core/DataSlowProvider";
 
 const STYLE_PAGE_HEADER = {
   SELECTOR: {
-    position: "sticky",
+    position: "fixed",
     top: 0,
     right: 0,
     left: 0,

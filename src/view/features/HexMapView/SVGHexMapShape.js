882ca52dfdf51acfc35708f2491a8e52463de9d9
nuuuwan <nuuuwan@gmail.com>
2024-10-08 13:19:05 +0530

    lint (SVGHexMapShape)

--- src/view/features/HexMapView/SVGHexMapShape.js
+++ src/view/features/HexMapView/SVGHexMapShape.js
@@ -51,7 +51,6 @@ export default function SVGHexMapShape({
     baseResultList: electionDisplay.baseResultList,
     resultIdx: electionDisplay.resultIdx,
   });
-
   return (
     <g onClick={onClick}>
       <SVGHexPolygonGroup points={points} color={color} opacity={opacity} />

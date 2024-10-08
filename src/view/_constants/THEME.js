d1713c550806f4f74c670c32001b2db94f8ae5f1
nuuuwan <nuuuwan@gmail.com>
2024-10-08 15:00:08 +0530

    various

--- src/view/_constants/THEME.js
+++ src/view/_constants/THEME.js
@@ -4,7 +4,7 @@ import CustomURLContext from "../../nonview/core/CustomURLContext";
 function getThemeData() {
   const lang = CustomURLContext.get().lang;
 
-  let fontFamily = "Cairo";
+  let fontFamily = "Afacad";
   let fontSize = 10;
   if (lang === "si") {
     fontFamily = "Noto Sans Sinhala";

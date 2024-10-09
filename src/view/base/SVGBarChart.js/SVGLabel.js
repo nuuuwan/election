04cd4f6d9cd542c1f67e3a87f22c10223d988a17
nuuuwan <nuuuwan@gmail.com>
2024-10-09 15:07:21 +0530

    y={(y0 + height / 2) + fontSize * 0.05}

--- src/view/base/SVGBarChart.js/SVGLabel.js
+++ src/view/base/SVGBarChart.js/SVGLabel.js
@@ -4,7 +4,7 @@ export default function SVGLabel({ x0, y0, width, height, label }) {
   return (
     <text
       x={x0 + width / 2}
-      y={y0 + height / 2}
+      y={(y0 + height / 2) + fontSize * 0.05}
       textAnchor="middle"
       dominantBaseline="middle"
       fill="white"

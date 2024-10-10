36ca5be45ba08dc79d2a442047dd6dc167984f34
nuuuwan <nuuuwan@gmail.com>
2024-10-10 18:32:06 +0530

    lint only

--- src/nonview/core/DataProvider.js
+++ src/nonview/core/DataProvider.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, createContext, useContext } from "react"
+import React, { useState, useEffect, createContext, useContext } from "react";
 import Timer from "../base/Timer";
 import { CustomLoadingProgress } from "../../view";
 import { DataProviderUtils } from "..";

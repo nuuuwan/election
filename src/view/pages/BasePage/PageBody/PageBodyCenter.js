939422b361549a18b2c1d44e38d165037abf09a5
nuuuwan <nuuuwan@gmail.com>
2024-11-01 11:58:17 +0530

    added getTextLines for <ExternalMedia id="hexmap"

--- src/view/pages/BasePage/PageBody/PageBodyCenter.js
+++ src/view/pages/BasePage/PageBody/PageBodyCenter.js
@@ -1,12 +1,26 @@
 import { ExternalMedia } from '../../..';
+import { useDataContext } from '../../../../nonview/core/DataProvider';
 import HexagonClickAlert from '../../../core/HexagonClickAlert';
 import ResultsReleasedTitleNumber from '../../../core/ResultsReleasedTitleNumber';
 import HexMapView from '../../../features/HexMapView/HexMapView';
 
+function getTextLines({ data }) {
+  const { electionDisplay, pdIdx } = data;
+
+  const { nResultsTotal, nResultsReleased } =
+    electionDisplay.getNResultsReleasedAndTotal('LK', pdIdx);
+
+  return [
+    `#HexMap of All Results`,
+    `${nResultsReleased} of ${nResultsTotal} Results Released`,
+  ];
+}
+
 export default function PageBodyCenter() {
+  const data = useDataContext();
   return (
     <>
-      <ExternalMedia id="hexmap">
+      <ExternalMedia id="hexmap" textLines={getTextLines({ data })}>
         <ResultsReleasedTitleNumber />
         <HexMapView />
       </ExternalMedia>

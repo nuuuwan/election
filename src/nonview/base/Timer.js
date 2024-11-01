ca5715fb87b8ddb64f6532efe01991161bcc1e5b
nuuuwan <nuuuwan@gmail.com>
2024-11-01 09:37:13 +0530

    Removed log

--- src/nonview/base/Timer.js
+++ src/nonview/base/Timer.js
@@ -1,8 +1,6 @@
 import Time from './Time';
 
 export default class Timer {
-  static ERROR_MULT = 1;
-
   static getLogger(maxDelta, delta) {
     if (delta < maxDelta) {
       return null;
@@ -11,16 +9,8 @@ export default class Timer {
   }
 
   static async logAsync(name, maxDelta, callable) {
-    return Timer._logInternal(name, maxDelta, callable, true);
-  }
-
-  static log(name, maxDelta, callable) {
-    return Timer._logInternal(name, maxDelta, callable, false);
-  }
-
-  static async _logInternal(name, maxDelta, callable, isAsync) {
     const ut = Time.now().ut;
-    const output = isAsync ? await callable() : callable();
+    const output = await callable();
     const delta = Time.now().ut - ut;
 
     const logger = Timer.getLogger(maxDelta, delta);

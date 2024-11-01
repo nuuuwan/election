72b5607ab0ccaf699bd55145b3bd7d6cf8d02600
nuuuwan <nuuuwan@gmail.com>
2024-11-01 06:47:50 +0530

    Added party color to EntWithPartyView

--- src/view/core/EntViewPartyLogo.js
+++ src/view/core/EntViewPartyLogo.js
@@ -1,11 +1,13 @@
 import { Stack } from '@mui/material';
+import { Party } from '../../nonview';
 import { EntView, PartyLogo } from '..';
 
 export default function EntWithPartyView({ entID, partyID }) {
+  const party = Party.fromID(partyID);
   return (
     <Stack direction="row">
       <PartyLogo partyID={partyID} />
-      <EntView entID={entID} />
+      <EntView entID={entID} sx={{ color: party.color }} />
     </Stack>
   );
 }

be0a82b3f39ea440ba25cd9b3fb5eda51226d947
nuuuwan <nuuuwan@gmail.com>
2024-11-01 12:25:09 +0530

    Added getTextLines

--- src/view/pages/BasePage/PageBody/PageBodyRight.js
+++ src/view/pages/BasePage/PageBody/PageBodyRight.js
@@ -1,5 +1,8 @@
 import CustomStack from '../../../core/CustomStack';
 
+import { useDataSlowContext } from '../../../../nonview/core/DataSlowProvider';
+
+import { Format, Party } from '../../../../nonview';
 import ProjectionTitle, {
   ProjectionAlert,
   ProjectionErrorAlert,
@@ -11,7 +14,6 @@ import {
   ProjectionModelInfoView,
   ExternalMedia,
 } from '../../..';
-import { useDataSlowContext } from '../../../../nonview/core/DataSlowProvider';
 
 function PageBodyRightTypeSpecific() {
   const data = useDataSlowContext();
@@ -25,11 +27,62 @@ function PageBodyRightTypeSpecific() {
   return <ProjectionViewParliamentary />;
 }
 
+function getTextLinesForResult({ resultLK }) {
+  const partyToPVotesSortedOthered =
+    resultLK.partyToVotes.partyToPVotesSortedOthered;
+  let lines = [''];
+  const pVotesError = resultLK.partyToVotes.partyToPVotes[Party.ERROR.id] || 0;
+
+  for (const [partyID, pVotes] of Object.entries(partyToPVotesSortedOthered)) {
+    const party = Party.fromID(partyID);
+    lines.push(
+      `${party.emoji} ${Format.percentRange(pVotes, pVotes + pVotesError)} ${
+        party.xTag
+      }`,
+    );
+  }
+  return lines;
+}
+
+function getTextLines({ data }) {
+  const {
+    electionDisplay,
+    electionProjected,
+    electionProjectedWithError,
+    pdIdx,
+  } = data;
+
+  const entID = 'LK';
+  const { nResultsTotal, nResultsReleased } =
+    electionDisplay.getNResultsReleasedAndTotal(entID, pdIdx);
+  const nToGo = nResultsTotal - nResultsReleased;
+  const isComplete = nToGo === 0;
+  const election =
+    electionProjectedWithError || electionProjected || electionDisplay;
+  const resultLK = election.resultLK;
+
+  console.debug(resultLK.partyToVotes.partyToVotes[Party.ERROR.id]);
+
+  return [].concat(
+    isComplete
+      ? ['🏁 Final Result']
+      : ['🤖 Projected Final Result', `${nToGo} results left`],
+    getTextLinesForResult({ resultLK }),
+  );
+}
+
 export default function PageBodyRight() {
+  const data = useDataSlowContext();
+  if (!data) {
+    return <CustomLoadingProgress />;
+  }
   return (
     <CustomStack>
       <ProjectionModelInfoView>
-        <ExternalMedia id="projection-details">
+        <ExternalMedia
+          id="projection-details"
+          textLines={getTextLines({ data })}
+        >
           <ProjectionTitle />
           <PageBodyRightTypeSpecific />
         </ExternalMedia>

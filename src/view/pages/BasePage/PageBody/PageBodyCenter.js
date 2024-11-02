a46355b4f0165bf6382de2813d5830d8738497ef
nuuuwan <nuuuwan@gmail.com>
2024-11-02 13:14:16 +0530

    lint

--- src/view/pages/BasePage/PageBody/PageBodyCenter.js
+++ src/view/pages/BasePage/PageBody/PageBodyCenter.js
@@ -1,12 +1,10 @@
 import { ExternalMedia, PartyToWinsView } from '../../..';
 
-import { useDataContext } from '../../../../nonview/core/DataProvider';
 import HexagonClickAlert from '../../../core/HexagonClickAlert';
 import ResultsReleasedTitleNumber from '../../../core/ResultsReleasedTitleNumber';
 import HexMapView from '../../../features/HexMapView/HexMapView';
 
 export default function PageBodyCenter() {
-  const data = useDataContext();
   return (
     <>
       <ExternalMedia id="hexmap">

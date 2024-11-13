9b38448225edea4e73c0bfa66b6565944244f2f8
nuuuwan <nuuuwan@gmail.com>
2024-11-13 12:30:17 +0530

    Make seats Anti-Clockwise

--- src/view/features/SeatsHexMap/SVGSeatCircle.js
+++ src/view/features/SeatsHexMap/SVGSeatCircle.js
@@ -1,12 +1,12 @@
-import React from "react";
-import SVGSeatCircleCircle from "./SVGSeatCircleCircle";
-import SVGSeatCircleText from "./SVGSeatCircleText";
+import React from 'react';
+import SVGSeatCircleCircle from './SVGSeatCircleCircle';
+import SVGSeatCircleText from './SVGSeatCircleText';
 
 const R_CIRCLE = 0.33;
 const ALPHA0 = -Math.PI / 2;
 
 export default function SVGSeatCircle({ i, n, x, y, partyID, seats }) {
-  const alpha = ALPHA0 + ((n - i - 1) * 2 * Math.PI) / n;
+  const alpha = ALPHA0 - ((n - i - 1) * 2 * Math.PI) / n;
   const cx = x + R_CIRCLE * Math.cos(alpha);
   const cy = y + R_CIRCLE * Math.sin(alpha);
 

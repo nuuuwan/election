6313208e14338baee73bb58febb1fbfd00a18121
nuuuwan <nuuuwan@gmail.com>
2024-11-15 01:12:30 +0530

    SVGScatterChartFittedLine

---
+++ src/view/base/SVGScatterChart/SVGScatterChartFittedLine.js
@@ -0,0 +1,18 @@
+export function SVGScatterChartFittedLine({ m, c, boundParams }) {
+  const { xMin, xMax, transformX, transformY } = boundParams;
+  const x1 = transformX(xMin);
+  const x2 = transformX(xMax);
+  const y1 = transformY(m * xMin + c);
+  const y2 = transformY(m * xMax + c);
+  return (
+    <line
+      x1={x1}
+      y1={y1}
+      x2={x2}
+      y2={y2}
+      stroke="#888"
+      strokeWidth="0.005"
+      strokeDasharray="0.01 0.01"
+    />
+  );
+}
